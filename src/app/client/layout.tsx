0de579c5e877a1fda29157ce17dcb04b3a5fd6c3
Amirhossein ghanbarian <ghanbarianamirhossein@gmail.com>
2025-04-05 11:56:15 +0330

    feat:create header and titles and add some library

--- src/app/client/layout.tsx
+++ src/app/client/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../../styles/globals.css";
+import Header from "@/components/layout/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="fa" dir="rtl">
+      <body>
+        <Header/>
         {children}
       </body>
     </html>
--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-green-600">test</p>
+    <div></div>
   );
 }
