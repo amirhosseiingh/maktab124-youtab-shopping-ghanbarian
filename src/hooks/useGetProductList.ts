fe055720a8e60ccc4f2f0cfda03c69da9a22a521
Amirhossein ghanbarian <ghanbarianamirhossein@gmail.com>
2025-05-09 14:20:30 +0330

    Fix: Handle accessToken retrieval for client-side

--- src/hooks/useGetProductList.ts
+++ src/hooks/useGetProductList.ts
@@ -1,14 +1,24 @@
-
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import { API_KEY, BASE_URL } from '@/configs/envReader';
 import { ProductsResponse } from '@/types/productList';
+import { useEffect, useState } from 'react';
 
 const useProducts = (category: string | null) => {
-  const accessToken =
-    typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null;
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('accessToken');
+      setAccessToken(token);
+    }
+  }, []);
 
   const fetchProducts = async () => {
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+
     const { data } = await axios.get<ProductsResponse>(
       BASE_URL +
         `/api/records/products?filterKey=category&filterValue=${category}`,
@@ -23,7 +33,7 @@ const useProducts = (category: string | null) => {
   };
 
   return useQuery(['products', category], fetchProducts, {
-    enabled: !!category,
+    enabled: !!category && !!accessToken, // only enable query when category and accessToken are available
   });
 };
 
